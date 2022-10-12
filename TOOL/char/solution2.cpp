4fcc10b12f7e43ea5cc9ab72430bc22a56427185
lsyhahaha <987083256@qq.com>
2022-10-12 17:04:53 +0800

    吉带

--- TOOL/char/solution2.cpp
+++ TOOL/char/solution2.cpp
@@ -7,29 +7,50 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+typedef struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+}*Tree;
+
+Tree build(string qx, string zx){// 前序中序构建二叉树
+    if (qx.size() == 0) return NULL;
+    TreeNode *root = (TreeNode*) malloc(sizeof (TreeNode));
+    root->val = qx[0]-'0';
+
+    int pos = zx.find(qx[0]);
+    root->left = build(qx.substr(1, pos), zx.substr(0,pos));
+    root->right = build(qx.substr(pos+1), zx.substr(pos+1));
+
+    return root;
+}
+
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        // 小镇法官的度为n-1
-        vector<int> degrees(n + 1);
-        int people = 0;
-        for(int i = 0; i < n; i++){
-            people = trust[i][1];
-            degrees[people]++;
-        }
-
-        for(int i = 0; i < n; i++){
-            if(degrees[i] == n-1) return i;
-        }
-
-        return -1;
+    int ans = INT_MAX;
+    int pre=INT_MAX;
+    int minDiffInBST(TreeNode* root) {
+        // 前一个访问结点的值
+        // 前序遍历
+        if(root->left)
+            minDiffInBST(root->left);
+        // root
+        ans = min(ans, root->val-pre);
+        pre = root->val;
+        if(root->right)
+            minDiffInBST(root->right);
+        return ans;
     }
 };
 
+
 int main(){
+    TreeNode *root = build("1245367", "4251637");
     Solution a = Solution();
-    vector<vector<int>> trust = {{1,2}};
-    cout << a.findJudge(2, trust);
+    a.minDiffInBST(root);
 
     return 0;
 }
